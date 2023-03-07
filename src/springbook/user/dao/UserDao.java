e968187c8d493c72a7a1e064d7246b27f08cf7b2
momo990305@gmail.com <momo990305@gmail.com>
2023-03-07 23:01:41 +0900

    UserDao upload

---
+++ src/springbook/user/dao/UserDao.java
@@ -0,0 +1,46 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhsot/Toby");
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c= DriverManager.getConnection(
+                "jdbc:mysql://localhost/Toby");
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next(); // index 첫 번째로 이동, 처음 커서는 인덱스 위에 위치
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
