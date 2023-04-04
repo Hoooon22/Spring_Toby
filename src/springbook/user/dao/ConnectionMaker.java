063eb7ade63024d54e12a821c84833227468de95
momo990305@gmail.com <momo990305@gmail.com>
2023-04-05 01:59:21 +0900

    ConnectionMaker

--- src/springbook/user/dao/ConnectionMaker.java
+++ src/springbook/user/dao/ConnectionMaker.java
@@ -1,14 +1,9 @@
 package springbook.user.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public interface ConnectionMaker {
     public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
-
-public class DConnectionMaker implements ConnectionMaker {
-    public Connection makeConnection() throws ClassNotFoundException, SQLException {
-        // D 사의 독자적인 방법으로 Connection을 생성하는 코드
-    }
 }
